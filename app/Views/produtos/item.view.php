0ca53eb04387127a87023e738d16486b6c159159
Brihan Brandao <brihan17@gmail.com>
2024-01-03 00:21:51 -0300

    05

--- app/Views/produtos/item.view.php
+++ app/Views/produtos/item.view.php
@@ -1,6 +1,7 @@
 <div class="row">
     <div class="col-12">
         <!-- Default box -->
+        <form action="<?= action(\Controllers\Produtos::class, 'update', 'POST') ?>" method="post">
         <div class="card card-primary ">
             <div class="card-header cursor-pointer" data-card-widget="collapse">
                 <h3 class="card-title">Formulario de cadastrar Produtos</h3>
@@ -22,19 +23,37 @@
                     <input type="number" step="0.01" min="0" class="form-control"name='Valor_un' id="inputValor" 
                     placeholder="Valor do produto"value="<?=value($valor_un)?>"required>
                   </div>
+                  
+                  <div class="form-grupo">
+                    <label for="selectUnidade">Unidade de Medida</label>
+                    <?php component(\Components\Select::class)
+                      ->addAttr('class','form-control')
+                      ->addAttr('required','')
+                      ->addAttr('id','selectUnidade')
+                      ->addAttr('name','unidade_medida')
+                      ->setPlaceholder('Selecione uma unidade para o produto')
+                      ->addOption('Unidade','Unidade')
+                      ->addOption('Kilo', 'Kilo')
+                      ->addOption('Grama', 'Grama')
+                      ->show();?>
+                  </div>
 
                   <div class="form-check">
                     <input type="checkbox" class="form-check-input" id="checkDisponivel"name='disponivel'
-                    <?=(value($disponivel)==1)?'checked':''?>>
+                    <?=(value($disponivel)==0)?'checked':''?>>
                     <label class="form-check-label" for="checkDisponivel">Disponivel</label>
                   </div>
                 </div>
+                <input type="hidden" name="id" value="<?= value($id) ?>">
             <!-- /.card-body -->
             <div class="card-footer">
-                Footer
+               <a href="<?=action(\Controllers\Produtos::class)?>" class="btn btn-info"><i class="fas fa-undo-alt"></i>Voltar</a>
+               <button type="reset" class="btn btn-warning"><i class="fas fa-eraser"></i>Limpar</button>
+               <button type="submit" class="btn btn-primary float-right"><i class="fas fa-save"></i>Salvar</button>
             </div>
             <!-- /.card-footer-->
         </div>
+        </form>
         <!-- /.card -->
     </div>    
 </div>
